e92627b80cf17df2ee64f5c485e9b3fd5277865c
Adrian Booth <Adrian.booth17@gmail.com>
2015-11-17 17:15:24 +0000

    create and edit tests added and passing

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -50,22 +50,58 @@ describe AuthorsController do
 
 	describe 'POST #create' do 
 		context 'a successful create' do 
+
+			before do 
+				post :create, author: Fabricate.attributes_for(:author) #attributes_for returns a hash instead of an object. 
+			end
 			
 			it 'saves the new author object' do 
-				post :create, author: Fabricate.attributes_for(:author)
+				# require 'pry';binding.pry
 				expect(Author.count).to eq(1)
 			end 
 
 			it 'redirect to the show action' do 
-
+				expect(response).to redirect_to author_path(Author.first)
 			end 
 
 			it 'sets the success flash message' do 
-
+				expect(flash[:success]).to eq ("Author has been created")
 			end 
 		end 
 		
 		context 'an unsuccessful create' do 
+			
+			before do 
+				post :create, author: Fabricate.attributes_for(:author, first_name: nil)
+			end 
+
+			it 'does not save the new author object with invalid inputs' do 
+				expect(Author.count).to eq(0)
+			end 
+		
+			it 'renders the new template' do 
+				expect(response).to render_template :new
+			end 
+
+			it 'sets the failure flash message' do 
+				expect(flash[:danger]).to eq ("Author has not been created")
+			end 
+
+		end 
+
+	end 
+
+	describe 'GET #edit' do
+		let(:author) { Fabricate(:author) }
+
+		it 'finds the author with the given id and assigns to @author variable' do 
+			get :edit, id: author
+			expect(assigns(:author)).to eq(author)
+		end 
+
+		it 'renders the edit template' do 
+			get :edit, id: author
+			expect(response).to render_template :edit
 
 		end 
 
